f239ecbf10a476c347ba33b9c5d459eab06d49f5
vikendra <vikendrajangid@gmail.com>
2026-04-19 19:14:26 +0530

    making changes in cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://expence-frontend-bb88h4a4j-vikendras-projects.vercel.app/"
+    "https://expence-frontend-bb88h4a4j-vikendras-projects.vercel.app"
   ],
   credentials: true
 }));
